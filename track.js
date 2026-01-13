cc8fd0b32bd4a2f5748980573bd260cc80bc3534
hacker114 <orson.sander@icloud.com>
2026-01-13 09:11:19 -0800

    Create track.js

---
+++ track.js
@@ -0,0 +1,5 @@
+<script async src="https://rum.cronitor.io/script.js"></script>
+<script>
+    window.cronitor = window.cronitor || function() { (window.cronitor.q = window.cronitor.q || []).push(arguments); };
+    cronitor('config', { clientKey: '07b2cbafbe02ed0f9de59ac7dd4df973' });
+</script>
